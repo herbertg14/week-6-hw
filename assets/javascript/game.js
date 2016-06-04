f408040b176c35b18781703eb5d60e06c4a102eb
herbertg14 <herbert.gutierrez@utexas.edu>
2016-06-04 09:58:26 -0500

    got the paused state to work

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,7 +14,7 @@ $("#searchButton").on("click",function(){
 $(document).on('click', '.animalButton',function() {
 
 	$("#gifs").empty();
-	
+
 	var animal = $(this).text();
 	console.log(animal);
 	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=dc6zaTOxFJmzC&limit=10";
@@ -28,15 +28,18 @@ $(document).on('click', '.animalButton',function() {
 	    	console.log(response);
 
 	        var results = response.data;
+	        var animalDiv = $('<div>');
 
 	        for (var i = 0; i < results.length; i++) {
-
-	            var animalDiv = $('<div>');
 
 	            var p = $('<p>').text("Rating: " + results[i].rating);
 
 	            var animalImage = $('<img>');
-	            animalImage.attr('src', results[i].images.fixed_height.url);
+	            animalImage.attr('src', results[i].images.fixed_height_still.url);
+	            animalImage.addClass('animalGif');
+	            animalImage.attr("data-state","still");
+	            animalImage.attr("data-still", results[i].images.fixed_height_still.url);
+	            animalImage.attr("data-animate",results[i].images.fixed_height.url);
 
 	            animalDiv.append(p);
 	            animalDiv.append(animalImage);
@@ -46,3 +49,16 @@ $(document).on('click', '.animalButton',function() {
 
 	    });
 	});
+
+$(document).on("click", ".animalGif", function(){
+	console.log(this);
+	var state = $(this).attr('data-state');
+	if ( state == 'still'){
+		$(this).attr('src', $(this).data('animate'));
+		$(this).attr('data-state', 'animate');
+	}
+	else{
+		$(this).attr('src', $(this).data('still'));
+		$(this).attr('data-state', 'still');
+	}
+})
